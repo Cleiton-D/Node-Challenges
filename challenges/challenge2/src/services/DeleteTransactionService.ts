6c23bf80de3f73e0d44045c2f7ce2012f73c83ed
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-05-26 00:38:16 -0400

    :sparkles: challenge finalized

--- challenges/challenge2/src/services/DeleteTransactionService.ts
+++ challenges/challenge2/src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(transaction_id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.findOne(transaction_id);
+    if (!transaction) {
+      throw new AppError('Transaction not exist.');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
